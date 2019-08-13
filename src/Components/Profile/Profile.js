c19fa9c1970ef841b0b2ad6c685c6f83ecaa532a
surabhi8 <surabhigupta2442@gmail.com>
2019-08-13 15:55:25 +0530

    add profile component

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import  './Profile.css';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+class Profile extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+    username:'',
+    password:'',
+    isLogggedIn:false,
+    companyObject:{},
+    isCompanyExist:false,
+    }
+   }
+  render(){
+    const {profileObject} = this.props;
+    return (
+      <div>
+      <MuiThemeProvider>
+          <AppBar
+             title="Profile"
+           />
+         </MuiThemeProvider>
+         <div className="profile-wrapper">
+         <div className="profile-attribute">Name:{profileObject.name}</div>
+         Company Name:
+         <Link 
+         to={{pathname:'/company-profile',aboutProps: {companyId:profileObject.companyId}}} 
+         className="nav-link">{profileObject.Company?profileObject.Company.name:null} </Link>
+          <div className="profile-attribute">Role: {profileObject.jobRole}</div>
+          </div>
+          </div>
+  );
+  }
+}
+
+export default Profile;
+
+Profile.propTypes = {
+  profileObject: PropTypes.object
+}
