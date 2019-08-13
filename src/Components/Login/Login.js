cc17ab3331990d94c87411449cefc0a52b8dfcf5
surabhi8 <surabhigupta2442@gmail.com>
2019-08-13 15:55:09 +0530

    add basic login component

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,80 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import React from 'react';
+import axios from 'axios';
+import Profile from '../Profile/Profile'
+class Login extends React.Component {
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:'',
+  isLogggedIn:false,
+  profileObject:{}
+  }
+ }
+ handleClick(event){
+  const apiBaseUrl = "http://localhost:3000/";
+  const payload={
+  "userName":this.state.username,
+  "password":this.state.password
+  }
+  axios.post(apiBaseUrl+'login',payload
+  ).then((response) => {
+  if(response.status === 200){
+    const payload = {userName:this.state.username}
+    axios.post(apiBaseUrl+'profile',payload).then((response)=>{
+      if(response.status===200){
+        this.setState({isLogggedIn:!this.state.isLogggedIn})
+        console.log("in login",response.data.result.Company.name);
+        this.setState({profileObject:response.data.result})
+      }
+    })
+  }
+  else if(response.status === 204){
+  console.log("Username password do not match");
+  alert("username password do not match")
+  } else {
+    console.log("Username does not exists");
+    alert("Username does not exist");
+  }
+  })
+  .catch(function (error) {
+  console.log(error);
+  });
+  }
+render() {
+    if(!this.state.isLogggedIn){
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  } else {
+    return<Profile profileObject={this.state.profileObject}/>
+    }
+} 
+}
+export default Login;
