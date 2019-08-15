f5d561db75f7149830b5efa3b257695e0d0ed6f0
surabhi8 <surabhigupta2442@gmail.com>
2019-08-15 11:05:28 +0530

    Updates

---
+++ precache-manifest.5ec3dcd7acde7e08ba61bc5b83443bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9210be956b0bd950ec7baddb1f81fd",
+    "url": "/job-profile-frontend/index.html"
+  },
+  {
+    "revision": "16fdb3c78fbe12857a50",
+    "url": "/job-profile-frontend/static/css/main.5ad6bccf.chunk.css"
+  },
+  {
+    "revision": "b0be37b8433cd51e1f45",
+    "url": "/job-profile-frontend/static/js/2.d13aedcf.chunk.js"
+  },
+  {
+    "revision": "16fdb3c78fbe12857a50",
+    "url": "/job-profile-frontend/static/js/main.098b1a37.chunk.js"
+  },
+  {
+    "revision": "ddde3848040a8238da28",
+    "url": "/job-profile-frontend/static/js/runtime~main.37d48b60.js"
+  }
+]);
